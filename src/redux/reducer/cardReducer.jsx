2c3a16cbe02f3d2ca9ef13cc9abc5cd74419ad4f
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-04 11:50:30 +0700

    refaktori reducer

---
+++ src/redux/reducer/cardReducer.jsx
@@ -0,0 +1,13 @@
+const cardState = {
+    data: {
+        nama: 'imdadu',
+        avatar: 'https://reqres.in/img/faces/12-image.jpg'
+    }
+}
+
+//reducer => fungsi yang berfungsi unutk mereturn state
+const cardReducer = (state = cardState) => {
+    return state
+}
+
+export default cardReducer;
---
+++ src/redux/reducer/navbarReducer.jsx
@@ -0,0 +1,12 @@
+const navbarState = {
+    data: {
+        nama_comp: 'IMDADU COMPANY',
+        logo: 'INI LOGO'
+    }
+}
+
+const navbarReducer = (state = navbarState) => {
+    return state;
+}
+
+export default navbarReducer

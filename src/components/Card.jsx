ed18248041ff3805133fe13fef9f2a33168449d5
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-09-04 13:02:09 +0700

    finish FD 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import CardImage from './CardImage'
 import CardName from './CardName'
 
 const Card = () => {
+    const {authReducer} = useSelector(a => a)
+    console.log(authReducer)
     return (
         <div>
-            <CardName />
-            <CardImage />
+            {
+                !! authReducer.isLogin ? (
+                    <div>
+                        <CardName />
+                        <CardImage />
+                    </div>
+                ) : (
+                    <h3>Anda belum login</h3>
+                )
+            }
+            
         </div>
     )
 }
--- src/redux/reducer/index.jsx
+++ src/redux/reducer/index.jsx
@@ -1,10 +1,13 @@
 import { combineReducers } from "redux";
 import cardReducer from "./cardReducer";
 import navbarReducer from "./navbarReducer";
+import authReducer from "./authReducer";
+
 
 const rooterReducer = combineReducers({
     cardReducer,
     navbarReducer,
+    authReducer,
 });
 
 export default rooterReducer
